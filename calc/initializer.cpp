cd2bbc8190ab9f3c1bf4905dbd41739faec31290
Eric <ebeecroft@mail.csuchico.edu>
2013-12-03 10:57:20 -0800

    Code performs cost calculation

--- calc/initializer.cpp
+++ calc/initializer.cpp
@@ -1,15 +1,15 @@
-//Prototype
+//Prototype #Abstraction Level 1
 int valuesetterfunc(char *argv[], int &counter); //Sets the variables to a read in variable from argv
 
-void initializerfunc(int &health, int &attack, int &defense, int &speed, char *argv[]); //Initializes variables
+void initializerfunc(int &health, int &attack, int &defense, int &speed, char *argv[]) //Initializes variables
 {
    //Count to be used only for value setter
    int count = 0;
    count = 1;
 
    //Set variables to ruby values
-   hp = valuesetterfunc(argv, count);
-   atk = valuesetterfunc(argv, count);
-   def = valuesetterfunc(argv, count);
-   spd = valuesetterfunc(argv, count);
+   health = valuesetterfunc(argv, count);
+   attack = valuesetterfunc(argv, count);
+   defense = valuesetterfunc(argv, count);
+   speed = valuesetterfunc(argv, count);
 }
