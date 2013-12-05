b40daeb2f1f90fd1fe5b158cca244cdd0db8c381
Eric <ebeecroft@mail.csuchico.edu>
2013-12-05 15:30:25 -0800

    Level calculations now work

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -2,20 +2,21 @@
 
 //Prototype #Abstraction Level 2
 void initializerfunc(int &health, int &attack, int &defense, int &speed, char *argv[]); //Initializes variables
-void calculatefunc(int health, int attack, int defense, int speed, int &cost); //Performs the calculations
+void calculatefunc(int health, int attack, int defense, int speed, int &cost, int &level); //Performs the calculations
 
 int main(int argc, char* argv[])
 {
    //Setup variables
-   int hp=0, atk=0, def=0, spd=0, cost=0;
-   
+   int hp=0, atk=0, def=0, spd=0, cost=0, level=0;
+   level = 1;   
+
    //Initializes the variables
    initializerfunc(hp, atk, def, spd, argv);
 
    //Perform calculation
-   calculatefunc(hp, atk, def, spd, cost);
+   calculatefunc(hp, atk, def, spd, cost, level);
 
    //Returns the values back to ruby
-   std::cout << cost;
+   std::cout << cost << "," << level;
    return 0;
 }
--- calc/calculate.cpp
+++ calc/calculate.cpp
@@ -1,8 +1,10 @@
 //Prototype #Abstraction Level 1
 int costcalculationfunc(int health, int attack, int defense, int speed); //Calculates the cost of a pet
+int levelcalculatefunc(int health, int attack, int defense, int speed); //Calculates the level of a pet
 
-void calculatefunc(int health, int attack, int defense, int speed, int &cost)
+void calculatefunc(int health, int attack, int defense, int speed, int &cost, int &level)
 {
    //Assigns the following variables a value
    cost = costcalculationfunc(health, attack, defense, speed);
+   level = levelcalculatefunc(health, attack, defense, speed);
 }
