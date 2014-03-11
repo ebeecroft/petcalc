0b9b6a6aa8c33263c20238432a055a76c0026e13
Eric <ebeecroft@mail.csuchico.edu>
2014-03-10 20:38:51 -0700

    Changed Cost Calculation to reflect level

--- calc/calculate.cpp
+++ calc/calculate.cpp
@@ -1,10 +1,10 @@
 //Prototype #Abstraction Level 1
-int costcalculationfunc(int health, int attack, int defense, int speed); //Calculates the cost of a pet
 int levelcalculatefunc(int health, int attack, int defense, int speed); //Calculates the level of a pet
+int costcalculationfunc(int level); //Calculates the cost of a pet
 
 void calculatefunc(int health, int attack, int defense, int speed, int &cost, int &level)
 {
    //Assigns the following variables a value
-   cost = costcalculationfunc(health, attack, defense, speed);
    level = levelcalculatefunc(health, attack, defense, speed);
+   cost = costcalculationfunc(level);
 }
--- calc/costcalculation.cpp
+++ calc/costcalculation.cpp
@@ -1,9 +1,9 @@
 #include <math.h>
 
-int costcalculationfunc(int health, int attack, int defense, int speed)
+int costcalculationfunc(int level)
 {
    //Sets the value of cost based on the stats of the pet
    int amount = 0;
-   amount = round((1+(health/10)+(attack/5)+(defense/5)+(speed/5))*10);
+   amount = round(level*40);
    return amount;
 }
